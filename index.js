b1a29c303f8638161a96af1a1aeefc3f68142650
salmaloum <goforsalma@gmail.com>
2023-01-29 08:07:38 -0800

    updated html

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 //required node modules
-const fs = require("fs");
-const inquirer = require("inquirer");
-const path = require("path");
+const fs = require('fs')
+const inquirer = require('inquirer')
+const path = require('path')
 
 //Adding Readme file
 //const Readme = require("./readme-generator");
 
 // importing team profiles
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
+const Manager = require('./lib/Manager')
+const Engineer = require('./lib/Engineer')
+const Intern = require('./lib/Intern')
 
 //teamMembers empty array
-const teamMembers = [];
+const teamMembers = []
 
 // linking to HTML template
-const templateHtml = require("./src/templateHTML");
+const templateHtml = require('./src/templateHTML')
 
 // Output Directory
-const DIST_DIR = path.resolve(__dirname, "dist");
+const DIST_DIR = path.resolve(__dirname, 'dist')
 
 // Output file path and name
-const outputPath = path.join(DIST_DIR, "teamProfile.html");
+const outputPath = path.join(DIST_DIR, 'teamProfile.html')
 
 //team members
 
@@ -29,23 +29,23 @@ function addMember() {
   inquirer
     .prompt([
       {
-        type: "list",
-        name: "what_team_member",
-        message: "Add a manager, an engineer, an intern to create the team",
-        choices: ["Manager", "Engineer", "Intern", "create the team"],
+        type: 'list',
+        name: 'what_team_member',
+        message: 'Add a manager, an engineer, an intern to create the team',
+        choices: ['Manager', 'Engineer', 'Intern', 'create the team'],
       },
     ])
     .then((answers) => {
-      if (answers.what_team_member === "Manager") {
-        addManager();
-      } else if (answers.what_team_member === "Engineer") {
-        addEngineer();
-      } else if (answers.what_team_member === "Intern") {
-        addIntern();
+      if (answers.what_team_member === 'Manager') {
+        addManager()
+      } else if (answers.what_team_member === 'Engineer') {
+        addEngineer()
+      } else if (answers.what_team_member === 'Intern') {
+        addIntern()
       } else {
-        createFile();
+        createFile()
       }
-    });
+    })
 }
 //creating job titles
 // Get manager data inputs
@@ -54,23 +54,23 @@ function addManager() {
   inquirer
     .prompt([
       {
-        type: "input",
-        name: "name",
-        message: "What is the name of the team manager?",
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of the team manager?',
       },
       {
-        type: "input",
-        name: "id",
-        message: "Employee ID of the team manager?",
+        type: 'input',
+        name: 'id',
+        message: 'Employee ID of the team manager?',
       },
       {
-        type: "input",
-        name: "email",
-        message: "Email address of the team manager?",
+        type: 'input',
+        name: 'email',
+        message: 'Email address of the team manager?',
       },
       {
-        type: "input",
-        name: "officeNumber",
+        type: 'input',
+        name: 'officeNumber',
         message: `What is the office number of the team manager?`,
       },
     ])
@@ -79,12 +79,12 @@ function addManager() {
         answers.name,
         answers.id,
         answers.email,
-        answers.officeNumber
-      );
-      console.table(manager);
-      teamMembers.push(manager);
-      addMember();
-    });
+        answers.officeNumber,
+      )
+      console.table(manager)
+      teamMembers.push(manager)
+      addMember()
+    })
 }
 
 //Get engineer data input//
@@ -92,23 +92,23 @@ function addEngineer() {
   inquirer
     .prompt([
       {
-        type: "input",
-        name: "name",
-        message: "What is the name of the team engineer?",
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of the team engineer?',
       },
       {
-        type: "input",
-        name: "id",
-        message: "Employee ID of the team engineer?",
+        type: 'input',
+        name: 'id',
+        message: 'Employee ID of the team engineer?',
       },
       {
-        type: "input",
-        name: "email",
-        message: "Email address of the team engineer?",
+        type: 'input',
+        name: 'email',
+        message: 'Email address of the team engineer?',
       },
       {
-        type: "input",
-        name: "gitHub",
+        type: 'input',
+        name: 'gitHub',
         message: `What is the engineer's github profile name?`,
       },
     ])
@@ -117,35 +117,35 @@ function addEngineer() {
         answers.name,
         answers.id,
         answers.email,
-        answers.gitHub
-      );
-      console.table(engineer);
-      teamMembers.push(engineer);
-      addMember();
-    });
+        answers.gitHub,
+      )
+      console.table(engineer)
+      teamMembers.push(engineer)
+      addMember()
+    })
 }
 //Get engineer data inputs //
 function addIntern() {
   inquirer
     .prompt([
       {
-        type: "input",
-        name: "name",
-        message: "What is the name of the team intern?",
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of the team intern?',
       },
       {
-        type: "input",
-        name: "id",
-        message: "Employee ID of the team intern?",
+        type: 'input',
+        name: 'id',
+        message: 'Employee ID of the team intern?',
       },
       {
-        type: "input",
-        name: "email",
-        message: "Email address of the team intern?",
+        type: 'input',
+        name: 'email',
+        message: 'Email address of the team intern?',
       },
       {
-        type: "input",
-        name: "school",
+        type: 'input',
+        name: 'school',
         message: `What school did the intern go to?`,
       },
     ])
@@ -154,28 +154,28 @@ function addIntern() {
         answers.name,
         answers.id,
         answers.email,
-        answers.school
-      );
-      console.table(intern);
-      teamMembers.push(intern);
-      addMember();
-    });
+        answers.school,
+      )
+      console.table(intern)
+      teamMembers.push(intern)
+      addMember()
+    })
 }
 //creating html file
 
 function createFile() {
   if (!fs.existsSync(DIST_DIR)) {
-    fs.mkdirSync(DIST_DIR);
+    fs.mkdirSync(DIST_DIR)
   } else {
-    fs.writeFileSync(outputPath, templateHtml(teamMembers), "utf-8");
-    console.log("HTML file created in the dist folder");
+    fs.writeFileSync(outputPath, templateHtml(teamMembers), 'utf-8')
+    console.log('HTML file created in the dist folder')
   }
 }
 
 //starting application
 
 function startApp() {
-  addManager();
+  addManager()
 }
 
-startApp();
+startApp()
--- src/templateHTML.js
+++ src/templateHTML.js
@@ -3,160 +3,153 @@
 const teamRender = (team) => {
   //creating manager card with user inputs
   const renderManager = (manager) => `
-        <div class="card col-3 me-3 mb-5 employee-card text-bg-dark">
-  
-        <div class="card-header employee-header">
-            <h2 class="card-title">${manager.name}</h2>
-            <h3 class="card-title">
-            <i class="bi bi-cup-hot"></i> ${manager.title}
-            </h3>
-        </div>
-        <div class="card-body">
-            <ul class="list-group">
-              <li class="list-group-item">ID: ${manager.id}</li>
-              <li class="list-group-item">
-                  Email: <a href="mailto: ${manager.email}">${manager.email}</a>
-              </li>
-              <li class="list-group-item">Office number: ${manager.officeNumber}</li>
-           </ul>
-        </div>
-        </div>
-      `;
+
+  <div
+  class="card text-white bg-dark mb-3 col-md-3"
+  style="max-width: 18rem;"
+>
+  <div id= "manager-card" class="card-header"><h1>Manager</h1></div>
+  <div class="card-body">
+  <h5 class="card-title">Name: ${manager.name}</h5>
+  <p class="card-text">
+      <ul class="list-group">
+      <li class="list-group-item list-group-item-success">Email:  <a href="mailto: ${manager.email}">${manager.email}</a> </li>
+      <li class="list-group-item list-group-item-success">ID: ${manager.id} </li>
+      <li class="list-group-item list-group-item-success">Office Number: ${manager.officeNumber} </li>
+      </ul>
+  </p>
+  </div>
+</div>
+      `
 
   //creating engineer card with user inputs
   const renderEngineer = (engineer) => `
-        <div class="card col-3 me-3 mb-5 employee-card text-bg-dark">
-        
-        <div class="card-header employee-header">
-            <h2 class="card-title">${engineer.name}</h2>
-            <h3 class="card-title"><i class="bi bi-gear"></i> ${engineer.title}</h3>
-        </div>
-        <div class="card-body">
-            <ul class="list-group">
-            <li class="list-group-item">ID: ${engineer.id}</li>
-            <li class="list-group-item">
-                Email: <a href="mailto:${engineer.email}">${engineer.email}</a>
-            </li>
-            <li class="list-group-item">
-                GitHub:
-                <a
-                href="https://github.com/${engineer.gitHub}"
-                target="_blank"
-                rel="noopener noreferrer"
-                >${engineer.gitHub}</a
-                >
-            </li>
-            </ul>
-        </div>
-        </div>
-      `;
+  <div
+  class="card text-white bg-dark mb-3 col-md-3"
+  style="max-width: 18rem;"
+  >
+  <div id= "engineer-card" class="card-header"><h1>Engineer</h1></div>
+  <div class="card-body">
+      <h5 class="card-title">Name: ${engineer.name}</h5>
+      <p class="card-text">
+      <ul class="list-group">
+      <li class="list-group-item list-group-item-success">ID: ${engineer.id} </li>
+      <li class="list-group-item list-group-item-success">Email:  
+      <a href="mailto:${engineer.email}">${engineer.email}</a> </li>
+      <li class="list-group-item list-group-item-success">Github:   
+      <a
+      href="https://github.com/${engineer.gitHub}"
+      target="_blank"
+      rel="noopener noreferrer"
+      >${engineer.gitHub}</a
+      >
+       </li>
+      </ul>
+      </p>
+  </div>
+  </div>
+      `
 
   //creating Intern card with user inputs
   const renderIntern = (intern) => `
-            <div class="card col-3 me-3 mb-5 employee-card text-bg-dark">
-           
-            <div class="card-header employee-header">
-                <h2 class="card-title">${intern.name}</h2>
-                <h3 class="card-title">
-                <i class="bi bi-mortarboard"></i> ${intern.title}
-                </h3>
-            </div>
 
-            <div class="card-body">
-                <ul class="list-group">
-                <li class="list-group-item">ID: ${intern.id}</li>
-                <li class="list-group-item">
-                    Email: <a href="mailto:${intern.email}">${intern.email}</a>
-                </li>
-                <li class="list-group-item">School: ${intern.school}</li>
-                </ul>
-            </div>
-            </div>
-      `;
+  <div
+  class="card text-white bg-dark mb-3 col-md-3"
+  style="max-width: 18rem;"
+>
+  <div id= "intern-card" class="card-header"><h1>Intern</h1></div>
+  <div class="card-body">
+  <h5 class="card-title"> Name: ${intern.name}</h5>
+  <p class="card-text">
+      <ul class="list-group">
+      <li class="list-group-item list-group-item-success"> Email:
+      <a href="mailto:${intern.email}">${intern.email}</a></li>
+      <li class="list-group-item list-group-item-success"> ID: ${intern.id} </li>
+      <li class="list-group-item list-group-item-success">School: ${intern.school} </li>
+      </ul>
+  </p>
+  </div>
+</div>
+      `
 
   // Joining and rendering HTML
   // Create an empty array for the html cards
-  const htmlCreate = [];
+  const htmlCreate = []
 
   // Add the manager to the empty array of HTML cards
   htmlCreate.push(
     team
-      .filter((employee) => employee.getRole() === "Manager")
-      .map((manager) => renderManager(manager))
-  );
+      .filter((employee) => employee.getRole() === 'Manager')
+      .map((manager) => renderManager(manager)),
+  )
 
   // Add the engineers to the array of HTML cards
   htmlCreate.push(
     team
       // filter out data so it only includes engineers
-      .filter((employee) => employee.getRole() === "Engineer")
+      .filter((employee) => employee.getRole() === 'Engineer')
       // Map all engineer data so that it displays as a html card
       .map((engineer) => renderEngineer(engineer))
       // Join all the cards up so that they're not on separate indexes
-      .join("")
-  );
+      .join(''),
+  )
 
   // Add the interns to the array of HTML cards
   htmlCreate.push(
     team
       // filter out data so it only includes interns
-      .filter((employee) => employee.getRole() === "Intern")
+      .filter((employee) => employee.getRole() === 'Intern')
       // Map all intern data so that it displays as a html card
       .map((intern) => renderIntern(intern))
       // Join all the cards up so that they're not on separate indexes
-      .join("")
-  );
+      .join(''),
+  )
 
   // Return the Completed HTML
-  return htmlCreate.join("");
-};
+  return htmlCreate.join('')
+}
 
 /* main html */
 const mainHTML = (team) => `
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-    <title>Meet My Team</title>
+        <title>Meet My Team</title>
 
-    <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-      integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi"
-      crossorigin="anonymous"
-    />
+        <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi"
+        crossorigin="anonymous"
+        />
 
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" hsref="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;600&display=swap"
-      rel="stylesheet"
-    />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" hsref="https://fonts.gstatic.com" crossorigin />
+        <link
+        href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;600&display=swap"
+        rel="stylesheet"
+        />
+
+        <link rel="stylesheet" href="./style.css" />
+    </head>
 
 
-    <link rel="stylesheet" href="./style.css" />
-  </head>
-
-  <body>
+    <body>
     <div class="container-fluid">
-      <div class="row">
-        <div class="team-profile-header col-12 mb-3 team-profile text-bg-warning">
-          <h1 class="team-profile-title text-center">Meet The Team</h1>
-        </div>
-      </div>
+    <nav class="navbar navbar-light bg-light">
+    <div class="container-fluid">
+        <span class="navbar-brand mb-0 h1">Meet The Team</span>
     </div>
-    <div class="container">
-      <div class="row">
-        <div
-          class="team-mem-container col-12 d-flex flex-wrap justify-content-around"
-        >
-          ${teamRender(team)}
-        </div>
-      </div>
+    </nav>
+
+    <div class="conatiner">
+    ${teamRender(team)}
     </div>
+
 
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
@@ -165,9 +158,7 @@ crossorigin="anonymous"></script>
 
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 
-<script src="https://kit.fontawesome.com/c502137733.js"></script>
-
 </html>
-`;
+`
 
-module.exports = mainHTML;
+module.exports = mainHTML
